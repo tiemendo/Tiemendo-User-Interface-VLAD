6b516d6af4b013749cc1eda09d83b61308f0b1c4
vladmog <vladmog@gmail.com>
2019-05-21 13:41:10 -0700

    worked around an issue that kept nav links hidden upon resize to desktop view

--- js/index.js
+++ js/index.js
@@ -7,16 +7,28 @@ class DropDownLink{
         this.button.addEventListener ("click", () => {
             this.toggleContent();
         })
+        this.addEventListener("resize", () => {
+            this.hideContent();
+        })
     }
     toggleContent(){
-        //this.content.classList.toggle("show"); //broken variation
-        //this.content.style.display = "flex"; //functional but insufficient variation\
-        if(this.content.style.display === "flex"){
-            console.log("if fork valid, code ineffective")
-            this.content.style.display = "none";
-        }else{
-            this.content.style.display = "flex";
-        }
+        this.content.classList.toggle("show"); // <<didn't work until !important added to .show class
+        //##########################################
+
+        //this.content.style.display = "flex"; // functional but insufficient variation
+
+        //###########################################
+
+        // if(this.content.style.display === "flex"){
+        //     this.content.style.display = "none";     // << works but nav stays hidden on resize
+        // }else{
+        //     this.content.style.display = "flex";
+        // }
+        
+        //###########################################
+    }
+    hideContent(){
+        this.content.classList.remove("show");
     }
 }
 
@@ -25,4 +37,9 @@ class DropDownLink{
 let dropDown = document.querySelectorAll(".navFunctional");
 dropDown.forEach(function(element){
     return new DropDownLink(element);
+})
+
+
+document.addEventListener("resize", function(){
+
 })
